73bf90d603163386dc566b5744936834440002e5
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2022-10-25 06:57:44 +0530

    Update log.controller.js

--- Back-end/controller/log.controller.js
+++ Back-end/controller/log.controller.js
@@ -17,18 +17,23 @@ router.post(
   async (req, res) => {
     try {
       const errors = validationResult(req);
+      //if the 'errors' is not empty ,we will return an error 
       if (!errors.isEmpty()) {
         res.status(404).send({ message: errors.array() });
       } else {
-        const regData = await Reg.findOne({ email: req.body.email });
+      //if there is no errors,we will check for a user ,from the email id provided.  
+        const regData = await Reg.findOne({ email: req.body.email });  
         if (!regData) {
           res.status(400).send("Email or password is wrong");
         } else {
+      //if the user is found using the email id ,then we will check weather the hashed passwords are equal     
           const match = regData.checkPassword(req.body.password);
           console.log(match);
+      //if not equal it will return false and we will send error    
           if (!match) {
             res.status(400).send("Email or password is wrong");
           } else {
+       //if it is true ,that means given password and the registered passwords matches=>so send jwt as response to the front-end    
             const token = newToken(regData);
             res.status(200).send({ token });
           }
