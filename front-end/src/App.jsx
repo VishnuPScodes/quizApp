d7c6ae7c9233f5af6e495f56059590d26a771dc9
VishnuPS <psvishnu373@gmail.com>
2023-02-23 15:01:28 +0530

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,10 +11,7 @@ function App() {
   return (
     <div className="App">
       <Allroutes />
-      {/* <Question/> */}
-     {/* <Log/> */}
-     {/* <Admin/> */}
-     {/* <QuizEnd/> */}
+     
     </div>
   );
 }
